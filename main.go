85c161234622d2ee3cc2f419e08b8dec593b02a1
Matt Cottingham <mattcottingham@gmail.com>
2018-05-29 14:58:37 +0100

    Skeleton handler which parses and prints webhook data

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	if req.HTTPMethod != "POST" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusMethodNotAllowed,
+			Body:       http.StatusText(http.StatusMethodNotAllowed),
+		}, nil
+	}
+
+	webhookBody := WebhookBody{}
+	if err := json.Unmarshal([]byte(req.Body), &webhookBody); err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       http.StatusText(http.StatusInternalServerError),
+		}, nil
+	}
+
+	log.Printf("%+v", webhookBody)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusAccepted,
+		Body:       http.StatusText(http.StatusAccepted),
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
---
+++ webhook.go
@@ -0,0 +1,42 @@
+package main
+
+import "time"
+
+type WebhookBody struct {
+	Type string
+	Data WebhookData
+}
+
+type WebhookData struct {
+	AccountID   string `json:"account_id"`
+	Amount      int    // Pennies
+	Created     time.Time
+	Currency    string
+	Description string
+	ID          string
+	Category    string
+	IsLoad      bool `json:"is_load"`
+	Settled     bool
+	Merchant    Merchant
+}
+
+type Merchant struct {
+	Address  Address
+	Created  time.Time
+	GroupID  string `json:"group_id"`
+	ID       string
+	Logo     string
+	Emoji    string
+	Name     string
+	Category string
+}
+
+type Address struct {
+	Address   string
+	City      string
+	Country   string
+	Latitude  float64
+	Longitude float64
+	Postcode  string
+	Region    string
+}
